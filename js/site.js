1d3bdd278f5ab2029e96debfbce161f22e743038
mbhavnag <mbhavnag@hawk.iit.edu>
2018-02-18 18:46:41 -0600

    Thanks is hidden instead of different page

--- js/site.js
+++ js/site.js
@@ -1,25 +1,6 @@
-$('.box').find('input, textarea').on('keyup blur focus', function(e){
-
-  var $this = $(this),
-      label = $this.prev('label');
-
-    if (e.this === 'keyup'){
-      if($this.val() === ''){
-        label.removeClass('active highlight');
-      } else {
-        label.addClass('active highlight');
-      }
-    }else if (e.type === 'blur'){
-      if($this.val() === ''){
-        label.removeClass('active highlight');
-      } else {
-        label.removeClass('highlight');
-      }
-    }else if (e.type === 'focus'){
-      if($this.val() === ''){
-        label.removeClass('highlight');
-      }else if($this.val() !== ''){
-        label.addClass('highlight');
-      }
-    }
+$(document).ready(function() {
+  $("#myform").submit(function(e){
+    $("#signup").hide();
+    $("#thanks").show();
+  });
 });
